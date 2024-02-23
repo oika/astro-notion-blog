a12969bd470fe7db239e86beb93328dca8e9edf4
akio <akio.akasaka@vetell.jp>
2024-02-23 12:16:27 +0900

    Fix type definition of request params

--- src/lib/notion/request-params.ts
+++ src/lib/notion/request-params.ts
@@ -21,16 +21,25 @@ export interface RetrieveBlockChildren {
 }
 
 export type PropertyFilterObject = {
-  property: string
+  property: 'Published'
   checkbox: CheckboxFilterCondition
 } | {
-  property: string
+  property: 'Date'
   date: DateFilterCondition
 } | {
-  property: string
+  property: 'Tags'
+  multi_select: MultiSelectFilterCondition
+} | {
+  property: 'Slug'
+  rich_text: TextFilterCondition
+} | {
+  property: 'Rank'
+  number: NumberFilterCondition
+} | {
+  timestamp: 'created_time'
   created_time: DateFilterCondition
 } | {
-  property: string
+  timestamp: 'last_edited_time'
   last_edited_time: DateFilterCondition
 }
 
@@ -44,6 +53,52 @@ export type CheckboxFilterCondition = {
   equals: boolean
 } | {
   does_not_equal: boolean
+}
+
+export type MultiSelectFilterCondition = {
+  contains: string
+} | {
+  does_not_contain: string;
+} | {
+  is_empty: true
+} | {
+  is_not_empty: true
+}
+
+export type TextFilterCondition = {
+  equals: string
+} | {
+  does_not_equal: string
+} | {
+  contains: string
+} | {
+  does_not_contain: string
+} | {
+  starts_with: string
+} | {
+  ends_with: string
+} | {
+  is_empty: true
+} | {
+  is_not_empty: true
+}
+
+export type NumberFilterCondition = {
+  equals: number
+} | {
+  does_not_equal: number
+} | {
+  greater_than: number
+} | {
+  less_than: number
+} | {
+  greater_than_or_equal_to: number
+} | {
+  less_than_or_equal_to: number
+} | {
+  is_empty: true
+} | {
+  is_not_empty: true
 }
 
 export type DateFilterCondition = {
@@ -63,6 +118,6 @@ export type DateFilterCondition = {
 }
 
 export interface PropertyValueSortObject {
-  property: string
+  property: 'Date' | 'Rank'
   direction: 'descending' | 'ascending'
 }
