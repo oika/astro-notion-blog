a2028d3b75a4d25902710e4b2192c585f29c1168
akio <akio.akasaka@vetell.jp>
2024-03-03 11:06:55 +0900

    multi lang: Headings of the side bar

--- src/content-constants.ts
+++ src/content-constants.ts
@@ -1,7 +1,36 @@
-export const HEADING = {
-    RECOMMENDED: "おすすめの記事",
-    RECENT: "最新の記事",
-    CATEGORIES: "カテゴリ",
-    SAME_CATEGORY: "同じカテゴリの記事"
+import type { AstroGlobal } from "astro";
 
+export const LANGUAGE_KEYS = [ "ja", "en" ] as const;
+
+export type LanguageKey = typeof LANGUAGE_KEYS[number];
+
+export const LANGUAGE: { [key in LanguageKey]: { dispName: string }} = {
+    ja: { dispName: "日本語" },
+    en: { dispName: "English" },
+} as const;
+
+export const Language = {
+    fromCurrentLocale: (astro: Pick<AstroGlobal, "currentLocale">): LanguageKey => {
+        const currentLocale = astro.currentLocale
+        if (currentLocale == null || !LANGUAGE_KEYS.some(lang => lang === currentLocale)) {
+            return "ja"
+        }
+
+        return currentLocale as LanguageKey
+    }
+}
+
+export const HEADING = {
+    ja: {
+        RECOMMENDED: "おすすめの記事",
+        RECENT: "最新の記事",
+        CATEGORIES: "カテゴリ",
+        SAME_CATEGORY: "同じカテゴリの記事"
+    },
+    en: {
+        RECOMMENDED: "Recommended",
+        RECENT: "Latest posts",
+        CATEGORIES: "Categories",
+        SAME_CATEGORY: "Posts in the same category"
+    }
 } as const;
